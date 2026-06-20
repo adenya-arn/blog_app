9fe231b85e1ef97e35365eb90aa1c7c0e8dfe0fe
adenya-arn <aadenya7@gmail.com>
2026-06-21 00:12:24 +0300

    DB working, Registering and login in, JWT is being generated too

--- blog_api/controllers/authController.js
+++ blog_api/controllers/authController.js
@@ -1,13 +1,103 @@
+import prisma from "../db/prisma.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export async function registerUser(req, res) {
   //   console.log(req.body);
 
-  const { username, email, password } = req.body;
+  // const { username, email, password } = req.body;
 
-  return res.json({ username, email, password });
+  // return res.json({ username, email, password });
+
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const existingUser = await prisma.user.findFirst({
+      where: { OR: [{ email }, { username }] },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Username or email already exists ",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(201).json({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      createdAt: user.createdAt,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
 }
 
 export async function loginUser(req, res) {
-  return res.json({
-    message: "login working",
-  });
+  // return res.json({
+  //   message: "login working",
+  // });
+
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+    );
+
+    return res.json({
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 }
--- blog_api/db/prisma.js
+++ blog_api/db/prisma.js
@@ -2,4 +2,5 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+console.log("works");
 export default prisma;
