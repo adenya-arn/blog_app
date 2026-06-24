d1644cb30c4e32f5192b4c6b5114ab5b49c50d69
adenya-arn <aadenya7@gmail.com>
2026-06-24 18:32:53 +0300

    Added loggin in and token verification

---
+++ blog_admin/src/components/AdminNavbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function AdminNavbar() {
+  function logout() {
+    localStorage.removeItem("token");
+
+    window.location.href = "/login";
+  }
+
+  return (
+    <nav id="admin-navbar">
+      <Link to="/">Dashboard</Link>
+
+      <Link to="/create-post">New Post</Link>
+
+      <button id="logout-btn" onClick={logout}>
+        Logout
+      </button>
+    </nav>
+  );
+}
---
+++ blog_admin/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/ Layout";
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <h1>Admin Dashboard</h1>
+
+      <p>Welcome to your blog admin panel.</p>
+    </Layout>
+  );
+}
